b0db86a026b149bcc5959d9249fc52f4abf7a9b7
DATBOI <alexsimons9999@gmail.com>
2020-01-11 14:52:14 -0600

    I has the right click menu action meow

---
+++ src/main/kotlin/io/acari/doki/actions/FileIconAssociation.kt
@@ -0,0 +1,13 @@
+package io.acari.doki.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAware
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+class FileIconAssociation : AnAction(), DumbAware {
+    override fun actionPerformed(e: AnActionEvent) { // TODO: insert action logic here
+    }
+}
